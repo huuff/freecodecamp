28eba5ce5d9159f8c1083517da5d9c7ddf2cb037
Haf <haf@protonmail.ch>
2021-06-16 17:17:03 +0200

    doing tribute with webpack

---
+++ responsive-web-design/tribute-v2/src/index.js
@@ -0,0 +1,2 @@
+import './styles.css'
+//import './assets/images/terry_davis.jpg'
---
+++ responsive-web-design/tribute-v2/webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      //{
+        //test: /\.html$/i,
+        //loader: 'html-loader',
+      //}
+    ],
+  },
+
+  plugins: [ new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "src", "index.html")
+  })],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 9000,
+  },
+
+}
