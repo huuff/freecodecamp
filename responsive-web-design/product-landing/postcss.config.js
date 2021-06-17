e021b443ccaf60fdf16b126b92c1a9c8daf4bb8c
Haf <haf@protonmail.ch>
2021-06-17 20:13:44 +0200

    added purgecss

--- responsive-web-design/product-landing/postcss.config.js
+++ responsive-web-design/product-landing/postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+  //plugins: {
+    //tailwindcss: {},
+    //autoprefixer: {},
+  //}
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './src/**/*.html'
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [] 
+    }),
+  ],
 }
